6bbd6349a5ff04063fa9396ad92433c33235f907
donnabillic <159816177+donnabillic@users.noreply.github.com>
2024-02-12 09:50:08 -0800

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "6562220687:AAGK25KffGgbreHjOAAK4FdsZP-7MrGn5K8",
-	chatId: "5200289419",
+	botToken: "6224306072:AAGA2_U2AIpjwHvcNUM2gzYP5dPHSeOrxe0",
+	chatId: "6091321941",
 };
